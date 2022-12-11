3312b31b656f9b63876090159042793e5911927d
Fernando Jr <120286349+Tugasdev@users.noreply.github.com>
2022-12-11 02:24:46 -0300

    Delete estudo .random1.py

--- estudo .random1.py
+++
@@ -1,3 +0,0 @@
-import random
-lista = random.choice(["Aninha", "Enzo", "Gustavo", "Pedro"])
-print (f"Hoje alunos, estarei sorteando 4 de vocês pra vir aqui resolver as atividades no quadro.\nEntre Pedro, Aninha, Enzo e Gustavo, vou escolher...\n{lista}")
