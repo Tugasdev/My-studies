7d7bf2e791ac37b3d297b6890d81770e2bf0636d
Fernando Jr <120286349+Tugasdev@users.noreply.github.com>
2022-12-11 02:23:49 -0300

    Create estudo .choice.py

---
+++ estudo .choice.py
@@ -0,0 +1,3 @@
+import random
+lista = random.choice(["Aninha", "Enzo", "Gustavo", "Pedro"])
+print (f"Hoje alunos, estarei sorteando 4 de vocês pra vir aqui resolver as atividades no quadro.\nEntre Pedro, Aninha, Enzo e Gustavo, vou escolher...\n{lista}")
