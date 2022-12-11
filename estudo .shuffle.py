ecb69561c0b9f5f1460e0a5906327905bfd9720c
Fernando Jr <120286349+Tugasdev@users.noreply.github.com>
2022-12-11 02:22:57 -0300

    Create estudo .shuffle.py

---
+++ estudo .shuffle.py
@@ -0,0 +1,4 @@
+import random
+lista = ["Aninha", "Enzo", "Gustavo", "Pedro"]
+random.shuffle (lista)
+print (f"Hoje alunos, estarei sorteando 4 de vocês pra resolver os exercícios em ordem. \nEntre Pedro, Aninha, Enzo e Gustavo, a ordem é...\n{lista}")
