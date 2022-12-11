dd8f8e5789ae831f93cb347049cee0f3d33b8d3f
Fernando Jr <120286349+Tugasdev@users.noreply.github.com>
2022-12-11 02:22:50 -0300

    Update conversor de medidas.py

--- conversor de medidas.py
+++ conversor de medidas.py
@@ -1,2 +1,2 @@
-metros = float(input("uma distância em metros"))
+metros = float(input("uma distância em metros: "))
 print (f"a medida de {metros}m corresponde a\n{metros/1000}km\n{metros/100}hm\n{metros/10}dam\n{metros*10}dm\n{metros*100}cm\n{metros*1000}mm")
