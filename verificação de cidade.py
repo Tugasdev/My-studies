c47a52c9c7e4a1c3e9358362f284a6a93a12d0b6
Fernando Jr <120286349+Tugasdev@users.noreply.github.com>
2022-12-13 08:59:52 -0300

    Create verificação de cidade.py

---
+++ verificação de cidade.py
@@ -0,0 +1,6 @@
+cidade=input ("digite o nome de uma cidade\n")
+cidade=cidade.lower()
+if ("santo" in cidade)==True:
+  print("essa cidade tem o nome 'Santo' nela.")
+else:
+  print("essa cidade não tem 'Santo' em seu nome.")
