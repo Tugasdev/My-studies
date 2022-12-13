163e596013146ba451079811672c88719fb73d6f
Fernando Jr <120286349+Tugasdev@users.noreply.github.com>
2022-12-13 08:59:54 -0300

    Create verificação de nome.py

---
+++ verificação de nome.py
@@ -0,0 +1,6 @@
+nome=input ("digite um nome\n")
+nome=nome.lower()
+if ("silva" in nome)==True:
+  print("esse nome tem o nome 'Silva' nela.")
+else:
+  print("esse nome não tem 'Silva' em seu nome.")
