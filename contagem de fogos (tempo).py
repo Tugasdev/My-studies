ff3f82f20cc915dd870a32ce3e4a0b3765b1c029
Fernando Jr <120286349+Tugasdev@users.noreply.github.com>
2022-12-14 19:46:53 -0300

    Create contagem de fogos (tempo).py

---
+++ contagem de fogos (tempo).py
@@ -0,0 +1,15 @@
+import time 
+ 
+print ("A CONTAGEM DOS FOGOS SE INICIA AGORA!!")
+time.sleep(3)
+
+for i in range(10,0,-1): 
+    if i==1:
+        print (f"{i}!!!!! :OOOOOO")
+    elif i<=3:
+        print (f"{i}!!!!!!")
+    else:
+        print(f"{i}!!!") 
+    time.sleep(1) 
+print ("FELIZ ANO NOVOOOOOOOOOOO")
+time.sleep(3)
