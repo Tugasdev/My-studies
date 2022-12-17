c81e5046858c7da1491a56e7d56d16fd59471002
Fernando Jr <120286349+Tugasdev@users.noreply.github.com>
2022-12-17 00:40:32 -0300

    Create printando numeros em tupla.py

---
+++ printando numeros em tupla.py
@@ -0,0 +1,15 @@
+from random import randint
+numeros = (randint (1,10), randint (1,10), randint (1,10), randint (1,10), randint (1,10))
+numero_maior= 0
+numero_menor= 10
+print (numeros)
+
+for i in numeros:
+    if i >= numero_maior:
+        numero_maior = i
+    if i <= numero_menor:
+        numero_menor = i
+
+print (f"O numero_maior é {numero_maior}, e o numero menor é {numero_menor}.")
+
+
